0a818e49bf4773522a654ef473d3f6310b70069c
leonkoech <leonkipkip@gmail.com>
2019-12-17 17:09:42 +0300

    automation

---
+++ scraper/backgroundruns.py
@@ -0,0 +1,32 @@
+import requests
+import json
+from bs4 import BeautifulSoup
+import webbrowser
+from notify_run import Notify
+from subprocess import call
+
+notify=Notify()
+def backgroundrun(current,dip,link,description):
+    #TO:DO 
+    # if current price is less than or equal to dip send notification
+    #start by checking the link and check the price
+    #check link
+    if(link!=''):
+        page=requests.get(link)
+        soup = BeautifulSoup(page.text, 'html.parser')
+        #check price
+        full_price = soup.find(class_="-b -ltr -tal -fs24").text.split(' ')
+        withcomma=full_price[1].strip().split(',')
+        pricek=withcomma[0].strip()
+        price1=full_price[1].strip()
+        price0=price1[-3:]
+        pricefull=pricek+price0
+        price=int(pricefull)
+        if (price==current):
+            notify.send('link is here',link)
+        if(price==dip or price<dip):
+            notify.send('PRICE DROP'+description,link)
+    else:
+        #if link is empty exit() or break
+        call('pkill -f backgroundruns.py',shell=True)
+        exit()
--- scraper/main.py
+++ scraper/main.py
@@ -1,18 +1,76 @@
 import requests
 import json
 from bs4 import BeautifulSoup
+import webbrowser
+import backgroundruns
+from notify_run import Notify
+from subprocess import call
 
+notify=Notify()
 sp='            '
 print('choose a category by number: \n [1] phones and tablets ',sp,'[2] electronics',sp,'[3] computing')
 print(' [4] home and office',sp,'[5] health and beauty',sp,'[6] grocery \n [7] carrefour shop',sp,'[8] baby products',sp,'[9] fashion')
-print(' [0] search whole website')
+print(' [0] search whole website',sp,'[11]observe product')
 category=int(input())
+#install pip install notify-run
+#call('pip3 install notify-run',shell=True)
+print('if prompted to Overwrite existing configuration press Y\n')
+call('notify-run register',shell=True)
+notifylink=notify.register().endpoint
+
+def productsearchWithPrice():
+    print("please enter the price of the product you've selected:\n")
+    price=int(input())
+    pricestr=str(price)
+    print('enter price you are waiting for:\n')
+    dip=int(input())
+    database = "data_file.json"
+    data = json.loads(open(database).read())
+    for i in data:
+        if i['price'] == pricestr:
+            print(i['brand'])
+            print(i['description'])
+            print(i['url'])
+            print('Is this the product?Y/n')
+            if(input().upper()=='Y'):
+                #user selected yes
+                print('click on subscribe  to receive a notification once the product of the price dips')
+                print('note: this process python code has to run in the background. so do not switch off your device.')
+                print('and if you do please rerun this python code')
+                print('and you can send this link to as many devices as needed')
+                print('\nopen link below\n',notifylink)
+                print('have you opened the link and subscribed?Y\n')
+                if(input().upper()=='Y'):
+                    call('chmod +x backgroundruns.py',shell=True)
+                    call('nohup python3 backgroundruns.py >/dev/null 2>&1 &',shell=True)
+                    backgroundruns.backgroundrun(price,dip,str(i['url']),str(i['description']))
+                    print('\n\nprocess started....\n\n')
+                    print('package will check product on background and will send notifications to subscribed devices')
+                    print('[1] kill every process \n[2]close python project while process runs')
+                    if(input()=='1'):
+                        call('pkill -f backgroundruns.py',shell=True)
+                        exit()
+                    elif(input()=='2'):
+                        exit()
+                if(input().lower()=='n'):
+                    print('\n\nError: Please open link and subscribe first')
+                    exit()
+            elif(input().lower()=='n'):
+                print('\n\nError: Wrong product detected')
+                exit()
+            else:
+                print('wrong input')
+                exit()
 def pageandsoup(page):
     soup = BeautifulSoup(page.text, 'html.parser')
     # print(soup)
     item = soup.find(class_="products -mabaya")
-    itemlist = item.find_all(class_="sku -gallery")
-    print('items count = ',len(itemlist))
+    try:
+        itemlist = item.find_all(class_="sku -gallery")
+    except AttributeError:
+        print('\n\nError: Product count is 0.\n       Please check the price range and retry')
+        exit()
+    print('product count = ',len(itemlist))
     data=[]
     for item in itemlist:
         Jmitem={}
@@ -39,23 +97,31 @@ def pageandsoup(page):
         print(price)
         data.append(Jmitem)
     with open("data_file.json", "w") as writeJSON:
-        json.dump(data, writeJSON,indent = 4, ensure_ascii=False,sort_keys=True)
+        json.dump(data, writeJSON,indent = 4, ensure_ascii=False,sort_keys=True) 
+    print('are these the products requested:Y/n')
+    if(input().capitalize()=='Y'):
+        productsearchWithPrice()
 def searchweb():
     print('enter product name:')
     querytxt = input()
-    print('enter maximum price of item')
+    print('enter maximum price of item WITHOUT COMMAS')
     #remove commas
-    maxJmprice = input().replace(',','')
+    maxPrice = int(input())
+    maxPricestr=str(maxPrice)
     #remove spaces
-    maxPrice=maxJmprice.strip()
-    print('enter minimum price of item')
-    minJmprice = input().replace(',','')
-    minPrice=minJmprice.strip()
-    query=querytxt.replace(' ','%20')
-    page = requests.get('https://www.jumia.co.ke/catalog/?q='+query+'&price='+minPrice+'-'+maxPrice)
-     
-    print('https://www.jumia.co.ke/catalog/?q='+query+'&price='+minJmprice+'-'+maxJmprice)
-    pageandsoup(page)
+    print('enter minimum price of item WITHOUT COMMAS')
+    minPrice = int(input())
+    minPricestr=str(minPrice)
+    if(maxPrice<minPrice):
+        print('\n\nError: Maximum price cannot be less than minimum price')
+        exit()
+    elif(maxPrice==minPrice):
+        print('Error: Maximum and minimum price cannot be equal')
+        exit()
+    else:
+        query=querytxt.replace(' ','%20')
+        page = requests.get('https://www.jumia.co.ke/catalog/?q='+query+'&price='+minPricestr+'-'+maxPricestr)
+        pageandsoup(page)
 def setpagerequest(link):
     print('enter maximum price of item')
     maxJmprice = input().replace(',','')
@@ -86,7 +152,12 @@ def switch_demo(argument):
         setpagerequest('fashion/'),
     elif(argument==0):
         searchweb(),
+    elif(argument==11):
+        searchweb(),
     else:
         print ("Invalid category.\nPlease try again")
 #this is called first
 switch_demo(category)
+
+
+    
